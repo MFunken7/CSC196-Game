3103ebecf5ae515a2f559ec2cdaaafee77069020
Michael <114317916+MFunken7@users.noreply.github.com>
2023-06-28 14:47:12 -0600

    new files

---
+++ Source/Engine/Core/memory.cpp
@@ -0,0 +1,16 @@
+#include "memory.h"
+#include <iostream>
+
+using namespace std;
+
+void* operator new(size_t size) {
+	cout << "allocated: " << size << endl;
+
+	return malloc(size);
+}
+
+void operator delete (void* address, size_t size) {
+	cout << "deallocated: " << size << endl;
+
+	free(address);
+}
--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 #include "../../Engine/Core/Random.h"
 #include "../../Engine/Core/FileIO.h"
+#include "../../Engine/Core/memory.h"
 using namespace std;
+
+
 
 
 int main()
 {
+	int* p = new int;
+	
+	delete p;
+
+
+
+
+
+
 
 	cout << kiko::getFilePath() << endl;
+	kiko::setFilePath("Assets");
+	cout << kiko::getFilePath() << endl;
+	size_t size;
+	kiko::getFileSize("game.txt", size);
+	cout << size << endl;
 
-	kiko::seedRandom((unsigned int)time(nullptr));
+	std::string s;
+	kiko::readFile("game.txt", s);
+	cout << s << endl;
+
+
+	/*kiko::seedRandom((unsigned int)time(nullptr));
 	for (int i = 0; i < 10; i++) {
 		cout << kiko::random(10, 20) << endl;
-	}
+	}*/
 }
